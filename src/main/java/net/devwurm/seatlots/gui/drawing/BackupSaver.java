bfde56c971acc500523805e8661d0a05cedbbcde
DevWurm <devwurm@gmx.net>
2016-04-20 13:53:05 +0200

    Adding BackupSaver function class

---
+++ src/main/java/net/devwurm/seatlots/gui/drawing/BackupSaver.java
@@ -0,0 +1,28 @@
+package net.devwurm.seatlots.gui.drawing;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import javafx.scene.control.Alert;
+import net.devwurm.seatlots.SeatLots;
+import net.devwurm.seatlots.location.RoomList;
+
+import java.util.prefs.Preferences;
+
+/**
+ * Class to save a RoomList state during drawing
+ */
+public class BackupSaver {
+    public static void saveState(RoomList roomListModel) {
+        Preferences store = Preferences.userNodeForPackage(SeatLots.class);
+
+        String modelJSON;
+        try {
+            modelJSON = roomListModel.toJSON();
+        } catch (JsonProcessingException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Fehler beim erzeugen der Konfigurationsdatei");
+            alert.showAndWait();
+            return;
+        }
+
+        store.put("backup_" + roomListModel.getName(), modelJSON);
+    }
+}
