abb4bf4f4be9ed5a9b305d662e5836dfe1e03ae6
DevWurm <devwurm@gmx.net>
2016-04-20 13:53:26 +0200

    Adding BackupLoader function class

---
+++ src/main/java/net/devwurm/seatlots/gui/drawing/BackupLoader.java
@@ -0,0 +1,54 @@
+package net.devwurm.seatlots.gui.drawing;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import javafx.scene.control.Alert;
+import net.devwurm.seatlots.SeatLots;
+import net.devwurm.seatlots.location.RoomList;
+import net.devwurm.seatlots.location.configuration.RoomListConfiguration;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * Class from loaing backup State files
+ */
+public class BackupLoader {
+    public static List<RoomList> loadStates() {
+        Preferences store = Preferences.userNodeForPackage(SeatLots.class);
+
+        try {
+            return Arrays.asList(store.keys()).stream()
+                    .filter(key -> key.contains("backup_"))
+                    .map(key -> store.get(key, ""))
+                    .map(json -> {
+                        RoomList roomListModel;
+                        try {
+                            roomListModel = RoomList.fromJSON(json);
+                        } catch (JsonParseException e) {
+                            Alert alert = new Alert(Alert.AlertType.ERROR, "Fehlerhafte Konfigurationsdatei");
+                            alert.showAndWait();
+                            return Optional.empty();
+                        } catch (IOException e) {
+                            Alert alert = new Alert(Alert.AlertType.ERROR, "Fehler beim Verarbeiten der Konfigurationsdatei");
+                            alert.showAndWait();
+                            return Optional.empty();
+                        }
+                        return Optional.of(roomListModel);
+                    })
+                    .filter(Optional::isPresent)
+                    .map(opRoomList -> (RoomList) opRoomList.get())
+                    .collect(Collectors.toList());
+        } catch (BackingStoreException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Fehler beim Lesen des Backup speichers");
+            alert.showAndWait();
+            return new ArrayList<>();
+        }
+    }
+}
